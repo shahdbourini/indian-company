de431b978a75736329c829eef56569ebb45008a4
Shahd Bourini <shahedal-boreeni@hotmail.com>
2021-05-11 02:02:43 +0300

    update

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
     <>
       <Router>
         <Switch>
-          <Route exact path="/">
+          <Route exact path="/indian-company">
             <Navbar blackColor="navbar-light" navLogo={blueLogo} />
             <Home />
           </Route>
