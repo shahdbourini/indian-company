b908a3a572403273c24cb1fdc147270a9d4a5330
Shahd Bourini <shahedal-boreeni@hotmail.com>
2021-05-29 16:34:49 +0300

    update HashRouter

--- src/App.js
+++ src/App.js
@@ -10,7 +10,12 @@ import Footer from './components/footer/Footer';
 import Navbar from './components/navbar/Navbar';
 import Login from './components/LogIn/Login';
 import Signup from './components/signup/SignUp';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  HashRouter,
+} from 'react-router-dom';
 import './App.css';
 import blueLogo from './images/blueLogo3.png';
 import whiteLogo from './images/logo3.png';
@@ -31,79 +36,78 @@ function App() {
 
   return (
     <>
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Navbar blackColor="navbar-light" navLogo={blueLogo} t={t} />
-            <Home t={t} />
-          </Route>
+      <HashRouter>
+        <Route exact path="/">
+          <Navbar blackColor="navbar-light" navLogo={blueLogo} t={t} />
+          <Home t={t} />
+        </Route>
 
-          <Route exact path="/business">
-            <Navbar
-              blackColor="navbar-dark blackColor"
-              navLogo={whiteLogo}
-              t={t}
-            />
-            <Business t={t} />
-          </Route>
+        <Route exact path="/business">
+          <Navbar
+            blackColor="navbar-dark blackColor"
+            navLogo={whiteLogo}
+            t={t}
+          />
+          <Business t={t} />
+        </Route>
 
-          <Route exact path="/about">
-            <Navbar blackColor="navbar-light" navLogo={blueLogo} t={t} />
-            <About t={t} />
-          </Route>
+        <Route exact path="/about">
+          <Navbar blackColor="navbar-light" navLogo={blueLogo} t={t} />
+          <About t={t} />
+        </Route>
 
-          <Route exact path="/features">
-            <Navbar
-              blackColor="navbar-dark blueColor"
-              navLogo={whiteLogo}
-              t={t}
-            />
-            <Features t={t} />
-          </Route>
-          <Route exact path="/contact">
-            <Navbar blackColor="navbar-light" navLogo={blueLogo} t={t} />
-            <Contact t={t} />
-          </Route>
+        <Route exact path="/features">
+          <Navbar
+            blackColor="navbar-dark blueColor"
+            navLogo={whiteLogo}
+            t={t}
+          />
+          <Features t={t} />
+        </Route>
+        <Route exact path="/contact">
+          <Navbar blackColor="navbar-light" navLogo={blueLogo} t={t} />
+          <Contact t={t} />
+        </Route>
 
-          {/* <Route exact path="/login">
+        {/* <Route exact path="/login">
             <Navbar blackColor="navbar-light" navLogo={blueLogo} t={t} />
             <Login />
           </Route> */}
 
-          <Route exact path="/sign-up">
-            <Navbar blackColor="navbar-light" navLogo={blueLogo} t={t} />
-            <Signup t={t} />
-          </Route>
+        <Route exact path="/sign-up">
+          <Navbar blackColor="navbar-light" navLogo={blueLogo} t={t} />
+          <Signup t={t} />
+        </Route>
 
-          <Route exact path="/instant-top-up">
-            <Navbar
-              blackColor="navbar-dark blueColor"
-              navLogo={whiteLogo}
-              t={t}
-            />
-            <Sec2 />
-          </Route>
+        <Route exact path="/instant-top-up">
+          <Navbar
+            blackColor="navbar-dark blueColor"
+            navLogo={whiteLogo}
+            t={t}
+          />
+          <Sec2 />
+        </Route>
 
-          <Route exact path="/manage-your-money">
-            <Navbar
-              blackColor="navbar-dark blueColor"
-              navLogo={whiteLogo}
-              t={t}
-            />
-            <Sec4 />
-          </Route>
+        <Route exact path="/manage-your-money">
+          <Navbar
+            blackColor="navbar-dark blueColor"
+            navLogo={whiteLogo}
+            t={t}
+          />
+          <Sec4 />
+        </Route>
 
-          <Route exact path="/instant-receive-and-send-payment">
-            <Navbar
-              blackColor="navbar-dark blueColor"
-              navLogo={whiteLogo}
-              t={t}
-            />
-            <Sec3 />
-          </Route>
-        </Switch>
+        <Route exact path="/instant-receive-and-send-payment">
+          <Navbar
+            blackColor="navbar-dark blueColor"
+            navLogo={whiteLogo}
+            t={t}
+          />
+          <Sec3 />
+        </Route>
+
         <Footer t={t} onChange={onChange} />
-      </Router>
+      </HashRouter>
     </>
   );
 }
